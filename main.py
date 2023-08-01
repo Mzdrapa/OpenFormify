589b537521bf34a6dfda03b0b5dc2f8bca8f88db
Pablo Andrés Úbeda Muñoz <drakoyuzz@gmail.com>
2023-08-01 13:05:43 +0200

    Initial Commit
    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Main
+import tkinter as tk
+from tkinter import ttk
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+def create_window():
+    root = tk.Tk()
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    create_window()
+    #print_hi('PyCharm')
